f606220e719f66fd4af847c3edfc9ca4e6a6519e
Alex Seif <alex.seif@gmail.com>
2022-12-13 16:49:20 +0200

    blog post

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -9,35 +9,46 @@
 defined( 'ABSPATH' ) || exit;
 ?>
 
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+<article <?php
+post_class(); ?> id="post-<?php
+the_ID(); ?>">
 
-	<header class="entry-header">
+    <header class="entry-header ayh-text-block" style="padding-top: 75px;">
+        <div class="meta-header">
+			<?php
+			the_category( ', ' ); ?>&nbsp;|&nbsp;<?php
+			the_date( 'd M y' ); ?>
+        </div>
+		<?php
+		the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+        <div class="entry-meta d-none">
 
-		<div class="entry-meta">
+			<?php
+			understrap_posted_on(); ?>
 
-			<?php understrap_posted_on(); ?>
+        </div><!-- .entry-meta -->
 
-		</div><!-- .entry-meta -->
+    </header><!-- .entry-header -->
 
-	</header><!-- .entry-header -->
+	<?php
+	echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
 
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
+    <div class="entry-content ayh-text-block">
 
 		<?php
 		the_content();
 		understrap_link_pages();
 		?>
 
-	</div><!-- .entry-content -->
+    </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+    <footer class="entry-footer">
 
-		<?php understrap_entry_footer(); ?>
+		<?php
+		understrap_entry_footer(); ?>
 
-	</footer><!-- .entry-footer -->
+    </footer><!-- .entry-footer -->
 
-</article><!-- #post-<?php the_ID(); ?> -->
+</article><!-- #post-<?php
+the_ID(); ?> -->
